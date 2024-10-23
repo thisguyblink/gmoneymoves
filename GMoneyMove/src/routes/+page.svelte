af9e770d1dd27b7da1dc99d9e9ca9bdde88dd0a9
thisguyblink <patrickliamsmith@gmail.com>
2024-10-23 11:48:15 -0700

    Base Website

--- GMoneyMove/src/routes/+page.svelte
+++ GMoneyMove/src/routes/+page.svelte
@@ -1,2 +1,45 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+</script>
+
+
+<body>
+    
+    <div class="Littles">
+        <h1> $$$    G Money Moves Line    $$$</h1>
+        <h2 class="Subtitle">Meet the Littles</h2>
+        <br>
+        
+        <div class="Angel">
+            
+        </div>
+        
+        <div class="Brian">
+            
+        </div>
+        
+        <div class="Cody">
+            
+        </div>
+        
+        <div class="Kevin">
+            
+        </div>
+        
+    </div>
+</body>
+
+
+<style>
+body {
+    background-color: #A9E5BB;
+}
+h1 {
+    /* color:  #708B75; */
+    background-color: #708B75;
+    border-radius: .25em;
+    text-align: center;
+    font-size: 3em;
+}
+
+
+</style>
---
+++ GMoneyMove/src/routes/navbar/+page.svelte
@@ -0,0 +1,37 @@
+<div class="nav">
+    <h1 id="title">G Money Moves</h1>
+    <div id="middle"></div>
+    <p class="categories">projects</p>
+</div>
+
+<slot></slot>
+
+
+
+<style>
+#title {
+    font-size: 2.5em;
+    color: #32533d;
+    padding-left: 5%;
+}
+
+.nav {
+    display: flex;
+    flex-direction: row;
+    max-height: 6em;
+    background-color: #BCFFDB;
+    border: solid 2px #4A2545;
+    border-radius: 1em;
+}
+
+#middle {
+    width: 40%;
+    align-items: center;
+    background-color: blue  ;
+}
+.categories {
+    background-color: aqua;
+    top: .5em;
+    align-self: start;
+}
+</style>
