fc6fdfa0d6c19483e4e5582a3d0e74b995b803cc
Kevin <Kevin-GeierConney>
2024-11-09 15:47:25 -0800

    added my page

--- GMoneyMove/src/routes/kevin/+page.svelte
+++ GMoneyMove/src/routes/kevin/+page.svelte
@@ -15,13 +15,23 @@
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Kevin's Page</title>
         </head>
+        
         <h1>Welcome to Kevin's Page</h1>
+        <h2>
+            <a href="/" class="backLink">Go Back</a>
+            or View my Projects
+        </h2>
     </main>
 
     <!-- Main Body-->
     <body>
-        <div>
+
+        <div class = "spookyBox">
+            <div class = "spooky">
+                <div> <a href="/kevin/spooky" class = "spookyLink"> SpookyHackAthon</a> </div>
+            </div>
         </div>
+
     </body>
 
 </html>
